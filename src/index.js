5c91a6d683a81090cdef5e533bac3bf84a3e4418
Xplor8r <mpieschel39@gmail.com>
2019-05-30 20:17:31 -0700

    install redux logger

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers/rootReducer'
 import thunk from 'redux-thunk'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import logger from 'redux-logger'
 
 const middlewares = [thunk]
 
